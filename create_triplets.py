7be62b8bc4c1bd4dce938c1be0a5d69c29de5111
Narges <41056415+narsisn@users.noreply.github.com>
2022-06-28 11:55:22 +0430

    Update create_triplets.py

--- create_triplets.py
+++ create_triplets.py
@@ -4,6 +4,10 @@ from itertools import combinations
 
 # read similar csv files 
 sim_file = pd.read_csv('csv_files/similar_images.csv')
+# remove multiple origin
+sim_file = sim_file.drop(sim_file[sim_file.image_list.str.count('origin')>1].index)
+# remove .gif files 
+sim_file = sim_file.drop(sim_file[sim_file.image_list.str.count('.gif')>0].index)
 
 
 train_file_path = 'csv_files/train_triplet.csv'
@@ -31,12 +35,13 @@ for index, row in sim_file.iterrows():
         sim_list = row['image_list'].replace(']','').replace('[','').replace("'",'').replace(' ','').split(',')
         # ignore .ong files 
         if '.png' in sim_list[0]:
+            i = i + 1
             continue 
         # remove .png files 
         sim_list = [row['image_path'] + '/' + img for img in sim_list if '.png' not in img]
         # generate combinition list from sim_list generate anch and pos images 
         com_list = list(combinations(sim_list,2))
-        df_tmp = sim_file.loc[(sim_file['main_category'] != row['main_category']) & (sim_file['gender'] != row['gender'])]
+        df_tmp = sim_file.loc[(sim_file['main_category'] != row['main_category']) & (sim_file['gender'] != row['gender']) & ('.png' not in row['image_list'])]
         # generatt neg images 
         df_tmp = df_tmp.sample(len(com_list))
         df_tmp['image_list'] = df_tmp.apply(add_path, axis = 1)
